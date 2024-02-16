750d87471ea997449ade6ffba5f15b618156d619
lumen-novum <novablackxd@gmail.com>
2024-02-16 00:15:50 -0600

    Added comment to server script

--- server-run.sh
+++ server-run.sh
@@ -1,1 +1,5 @@
+# This file serves as a reminder to run this project from the root directory, not whatever folder I'm currently in.
+
+# If not run from the root folder, there may be issues with Vapor Middleware
+
 run
